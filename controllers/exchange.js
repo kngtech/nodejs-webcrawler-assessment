a5a422c1f297a671f36b1802e51c171c8cf0e411
Gideon Aryeetey <gideonaryeetey8@gmail.com>
2022-06-07 22:51:34 +0000

    added: exchange rate converter implementation

--- controllers/exchange.js
+++ controllers/exchange.js
@@ -1,9 +1,32 @@
 const ExchangeRate = require('../models/ExchangeRate');
 
-const exchangeRate = async(req, res) => {
+const exchangeRate = async (req, res) => {
     try {
-        const exchange_rate = await ExchangeRate.find({})
-        res.status(200).json({exchange_rate})
+        const { from, amount } = req.query;
+        if (!amount || !(parseFloat(amount) > 0)) {
+            return res.status(400).json({ msg: 'Missing/Invalid amount query param. "amount" must be a number greater than zero (0)' })
+        }
+        if (!from) {
+            return res.status(400).json({ msg: 'Missing "from" query param. "from" refers to the currency code of the amount specified' })
+        }
+
+        const exchange_rate = await ExchangeRate.findOne({ currencyPair: `${from.toUpperCase()}GHS` })
+        if (!exchange_rate) {
+            return res.status(404).json({ msg: `Currency code ${from} not found` })
+        }
+
+        // Convert to cedis
+        const convertedAmount = {
+            selling: exchange_rate.selling * amount,
+            buying: exchange_rate.buying * amount,
+        }
+        res.status(200).json({
+            result: {
+                from: from.toUpperCase(),
+                amount: parseFloat(amount),
+                convertedAmount
+            }
+        })
     } catch (error) {
         res.status(500).json({ msg: error })
     }
